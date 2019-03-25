06949bbeca31fd836a89299b4512a30a7a3b537c
JP Cimalando <jp-dev@inbox.ru>
2019-03-25 23:44:59 +0100

    font map based on Xft

--- gdk/x11/gdkglfont-x11.c
+++ gdk/x11/gdkglfont-x11.c
@@ -18,7 +18,8 @@
 
 #include <string.h>
 
-#include <pango/pangox.h>
+#define PANGO_ENABLE_ENGINE 1
+#include <pango/pangoxft.h>
 
 #include "gdkglx.h"
 #include "gdkglprivate-x11.h"
@@ -29,44 +30,86 @@
 #endif /* GDKGLEXT_MULTIHEAD_SUPPORT */
 
 /*
- * This code is ripped from gdk/x11/gdkfont-x11.c in GTK+.
+ * This code is ripped from the FOX toolkit.
  */
-static char *
-gdk_gl_font_charset_for_locale (void)
-{
-  static char *charset_map[][2] = {
-    { "ANSI_X3.4-1968", "iso8859-1" },
-    { "US-ASCII",   "iso8859-1" },
-    { "ISO-8859-1", "iso8859-1" },
-    { "ISO-8859-2", "iso8859-2" },
-    { "ISO-8859-3", "iso8859-3" },
-    { "ISO-8859-4", "iso8859-4" },
-    { "ISO-8859-5", "iso8859-5" },
-    { "ISO-8859-6", "iso8859-6" },
-    { "ISO-8859-7", "iso8859-7" },
-    { "ISO-8859-8", "iso8859-8" },
-    { "ISO-8859-9", "iso8859-9" },
-    { "UTF-8",      "iso8859-1" }
-  };
+static void glXUseXftFont(XftFont* font,int first,int count,int listBase){
+  GLint swapbytes,lsbfirst,rowlength,skiprows,skippixels,alignment,list;
+  GLfloat x0,y0,dx,dy;
+  FT_Face face;
+  FT_Error err;
+  int i,size,x,y;
+  unsigned char *glyph;
 
-  const char *codeset;
-  char *result = NULL;
-  gsize i;
+  // Save the current packing mode for bitmaps
+  glGetIntegerv(GL_UNPACK_SWAP_BYTES,&swapbytes);
+  glGetIntegerv(GL_UNPACK_LSB_FIRST,&lsbfirst);
+  glGetIntegerv(GL_UNPACK_ROW_LENGTH,&rowlength);
+  glGetIntegerv(GL_UNPACK_SKIP_ROWS,&skiprows);
+  glGetIntegerv(GL_UNPACK_SKIP_PIXELS,&skippixels);
+  glGetIntegerv(GL_UNPACK_ALIGNMENT,&alignment);
 
-  g_get_charset (&codeset);
-  
-  for (i = 0; i < G_N_ELEMENTS (charset_map); i++)
-    if (strcmp (charset_map[i][0], codeset) == 0)
-      {
-	result = charset_map[i][1];
-	break;
+  // Set desired packing modes
+  glPixelStorei(GL_UNPACK_SWAP_BYTES,GL_FALSE);
+  glPixelStorei(GL_UNPACK_LSB_FIRST,GL_FALSE);
+  glPixelStorei(GL_UNPACK_ROW_LENGTH,0);
+  glPixelStorei(GL_UNPACK_SKIP_ROWS,0);
+  glPixelStorei(GL_UNPACK_SKIP_PIXELS,0);
+  glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+
+  // Get face info
+  face=XftLockFace(font);
+
+  // Render font glyphs; use FreeType to render to bitmap
+  for(i=first; i<count; i++){
+    list=listBase+i;
+
+    // Load glyph
+    err=FT_Load_Glyph(face,FT_Get_Char_Index(face,i),FT_LOAD_DEFAULT);
+    if(err){ g_warning("glXUseXftFont: unable to load glyph"); return; }
+
+    // Render glyph
+    err=FT_Render_Glyph(face->glyph,FT_RENDER_MODE_MONO);
+    if(err){ g_warning("glXUseXftFont: unable to render glyph"); return; }
+
+    // Pitch may be negative, its the stride between rows
+    size=ABS(face->glyph->bitmap.pitch) * face->glyph->bitmap.rows;
+
+    // Glyph coordinates; note info in freetype is 6-bit fixed point
+    x0=-(face->glyph->metrics.horiBearingX>>6);
+    y0=(face->glyph->metrics.height-face->glyph->metrics.horiBearingY)>>6;
+    dx=face->glyph->metrics.horiAdvance>>6;
+    dy=0;
+
+    // Allocate glyph data
+    glyph=g_malloc(size);
+
+    // Copy into OpenGL bitmap format; note OpenGL upside down
+    for(y=0; y<face->glyph->bitmap.rows; y++){
+      for(x=0; x<face->glyph->bitmap.pitch; x++){
+        glyph[y*face->glyph->bitmap.pitch+x]=face->glyph->bitmap.buffer[(face->glyph->bitmap.rows-y-1)*face->glyph->bitmap.pitch+x];
+        }
       }
 
-  if (result != NULL)
-    return g_strdup (result);
-  else
-    return g_strdup ("iso8859-1");
-}
+    // Put bitmap into display list
+    glNewList(list,GL_COMPILE);
+    glBitmap(ABS(face->glyph->bitmap.pitch)<<3,face->glyph->bitmap.rows,x0,y0,dx,dy,glyph);
+    glEndList();
+
+    // Free glyph data
+    g_free(glyph);
+    }
+
+  // Restore packing modes
+  glPixelStorei(GL_UNPACK_SWAP_BYTES,swapbytes);
+  glPixelStorei(GL_UNPACK_LSB_FIRST,lsbfirst);
+  glPixelStorei(GL_UNPACK_ROW_LENGTH,rowlength);
+  glPixelStorei(GL_UNPACK_SKIP_ROWS,skiprows);
+  glPixelStorei(GL_UNPACK_SKIP_PIXELS,skippixels);
+  glPixelStorei(GL_UNPACK_ALIGNMENT,alignment);
+
+  // Unlock face
+  XftUnlockFace(font);
+  }
 
 static PangoFont *
 gdk_gl_font_use_pango_font_common (PangoFontMap               *font_map,
@@ -76,11 +119,7 @@ gdk_gl_font_use_pango_font_common (PangoFontMap               *font_map,
                                    int                         list_base)
 {
   PangoFont *font = NULL;
-  gchar *charset = NULL;
-  PangoXSubfont subfont_id;
-  gchar *xlfd = NULL;
-  PangoXFontCache *font_cache;
-  XFontStruct *fs;
+  XftFont *xfont = NULL;
 
   GDK_GL_NOTE_FUNC_PRIVATE ();
 
@@ -91,37 +130,17 @@ gdk_gl_font_use_pango_font_common (PangoFontMap               *font_map,
       goto FAIL;
     }
 
-  charset = gdk_gl_font_charset_for_locale ();
-  if (!pango_x_find_first_subfont (font, &charset, 1, &subfont_id))
+
+  xfont = pango_xft_font_get_font(font);
+  if (xfont == NULL)
     {
-      g_warning ("cannot find PangoXSubfont");
-      font = NULL;
+      g_warning ("cannot load XftFont");
       goto FAIL;
     }
 
-  xlfd = pango_x_font_subfont_xlfd (font, subfont_id);
-  if (xlfd == NULL)
-    {
-      g_warning ("cannot get XLFD");
-      font = NULL;
-      goto FAIL;
-    }
-
-  font_cache = pango_x_font_map_get_font_cache (font_map);
-
-  fs = pango_x_font_cache_load (font_cache, xlfd);
-
-  glXUseXFont (fs->fid, first, count, list_base);
-
-  pango_x_font_cache_unload (font_cache, fs);
+  glXUseXftFont (xfont, first, count, list_base);
 
  FAIL:
-
-  if (charset != NULL)
-    g_free (charset);
-
-  if (xlfd != NULL)
-    g_free (xlfd);
 
   return font;
 }
@@ -149,11 +168,8 @@ gdk_gl_font_use_pango_font (const PangoFontDescription *font_desc,
 
   GDK_GL_NOTE_FUNC ();
 
-#ifdef GDKGLEXT_MULTIHEAD_SUPPORT
-  font_map = pango_x_font_map_for_display (GDK_DISPLAY_XDISPLAY (gdk_display_get_default ()));
-#else  /* GDKGLEXT_MULTIHEAD_SUPPORT */
-  font_map = pango_x_font_map_for_display (gdk_x11_get_default_xdisplay ());
-#endif /* GDKGLEXT_MULTIHEAD_SUPPORT */
+  font_map = pango_xft_get_font_map (gdk_x11_get_default_xdisplay (),
+                                     gdk_x11_get_default_screen ());
 
   return gdk_gl_font_use_pango_font_common (font_map, font_desc,
                                             first, count, list_base);
@@ -187,7 +203,8 @@ gdk_gl_font_use_pango_font_for_display (GdkDisplay                 *display,
 
   GDK_GL_NOTE_FUNC ();
 
-  font_map = pango_x_font_map_for_display (GDK_DISPLAY_XDISPLAY (display));
+  font_map = pango_xft_get_font_map (gdk_x11_display_get_xdisplay (display),
+                                     gdk_x11_screen_get_screen_number (gdk_display_get_default_screen (display)));
 
   return gdk_gl_font_use_pango_font_common (font_map, font_desc,
                                             first, count, list_base);
